626477b9e54ff9f45cec2ac89ae2bb856caf49c3
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2019-12-20 22:22:01 +0100

    ParseException inherited from runtime error

--- include/ParseException.h
+++ include/ParseException.h
@@ -2,12 +2,7 @@
 
 #include <exception>
 
-class ParseException : public std::exception {
-    const char* message;
+class ParseException : public std::runtime_error {
 public:
-    ParseException(const char* message) : message(message) {}
-
-    const char * what () const throw() override {
-        return message;
-    }
+    explicit ParseException(const char *message) : runtime_error(message) {}
 };
