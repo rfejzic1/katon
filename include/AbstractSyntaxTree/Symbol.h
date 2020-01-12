0f79f9c98a3b673c60b2b3873b8750b7e7941231
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-01-12 21:52:16 +0100

    Added scope information to Symbol

--- include/AbstractSyntaxTree/Symbol.h
+++ include/AbstractSyntaxTree/Symbol.h
@@ -3,10 +3,16 @@
 #include "Type.h"
 #include "Expression.h"
 
+enum class Scope {
+    Public, Private
+};
+
 struct Symbol : public Expression {
     std::string name;
     bool constant;
+    Scope scope;
     Type type;
 public:
-    Symbol(Identifier name, bool constant, Type type) : name(std::move(name)), constant(constant), type(type) {}
+    Symbol(Identifier name, bool constant, Type type, Scope scope = Scope::Public)
+        : name(std::move(name)), constant(constant), type(type), scope(scope) {}
 };
