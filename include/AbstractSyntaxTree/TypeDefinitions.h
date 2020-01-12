fde3d3a7c3976edcc1d30c630079a15c402a7f4a
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-01-13 00:08:55 +0100

    Added ExpressionList typedef to TypeDefinitions

--- include/AbstractSyntaxTree/TypeDefinitions.h
+++ include/AbstractSyntaxTree/TypeDefinitions.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 struct Value;
+struct Expression;
 
 template <typename T>
 using ptr = std::shared_ptr<T>;
@@ -11,3 +12,4 @@ using ptr = std::shared_ptr<T>;
 typedef std::string Identifier;
 typedef std::vector<ptr<Value>> ValueList;
 typedef std::vector<Identifier> IdentifierList;
+typedef std::vector<ptr<Expression>> ExpressionList;
