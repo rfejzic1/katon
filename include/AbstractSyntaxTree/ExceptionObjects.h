5c414d98814f8709c6ffa30d1186185e4e1e76fc
Rijad Fejzic <rfejzic1@etf.unsa.ba>
2020-01-21 23:24:27 +0100

    ExceptionObjects methods implemented

--- include/AbstractSyntaxTree/ExceptionObjects.h
+++ include/AbstractSyntaxTree/ExceptionObjects.h
@@ -2,9 +2,9 @@
 
 #include "TypeDefinitions.h"
 
-class Object;
-
-struct ExceptionObjects {
+class ExceptionObjects {
+    static ptr<Value> simple_exception(const std::string& type, const std::string& message);
+public:
     static ptr<Value> null_value(const std::string& message);
     static ptr<Value> bad_cast(const std::string& message);
     static ptr<Value> undefined_operation(const std::string& message);
--- src/ExceptionObjects.cpp
+++ src/ExceptionObjects.cpp
@@ -1,16 +1,24 @@
 #include "../include/AbstractSyntaxTree/ExceptionObjects.h"
 
-#include "../include/AbstractSyntaxTree/Values/Object.h"
 #include "../include/AbstractSyntaxTree/Values/String.h"
+#include "../include/AbstractSyntaxTree/ObjectBuilder.h"
+#include "../include/AbstractSyntaxTree/TypeDefinitions.h"
 
 ptr<Value> ExceptionObjects::null_value(const std::string &message) {
-    return ptr<Value>();
+    return simple_exception("null_value", message);
 }
 
 ptr<Value> ExceptionObjects::bad_cast(const std::string &message) {
-    return ptr<Value>();
+    return simple_exception("bad_cast", message);
 }
 
 ptr<Value> ExceptionObjects::undefined_operation(const std::string &message) {
-    return ptr<Value>();
+    return simple_exception("undefined_operation", message);
+}
+
+ptr<Value> ExceptionObjects::simple_exception(const std::string &type, const std::string &message) {
+    ObjectBuilder builder;
+    builder.putAttribute("type", true, make<String>(type));
+    builder.putAttribute("message", true, make<String>(message));
+    return builder.build();
 }
